58cbb4a5bba890a4b4a0f6e76667212344c5b357
Faisaljaved <faisaljaved06@gmail.com>
2017-03-06 15:36:20 +0500

    InitialCommit

---
+++ Assets/Scripts/BunnyBehave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehave : MonoBehaviour {
+	private Rigidbody2D myRigidBunny;
+	public float jumpForce = 500f;
+	// Use this for initialization
+	void Start () {
+		myRigidBunny = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("jump")) {
+			myRigidBunny.AddForce (transform.up * jumpForce);
+		}
+	}
+}
