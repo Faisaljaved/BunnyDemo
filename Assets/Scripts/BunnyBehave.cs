9056e6861e3ced3717b21d18ec937baaeea5a64f
Faisaljaved <faisaljaved06@gmail.com>
2017-03-12 14:56:52 +0500

    DeathImprove

--- Assets/Scripts/BunnyBehave.cs
+++ Assets/Scripts/BunnyBehave.cs
@@ -7,24 +7,43 @@ public class BunnyBehave : MonoBehaviour {
 	private Rigidbody2D myRigidBunny;
 	public float jumpForce = 500f;
 	private Animator myAnim;
+	private float bunnyHurtTime = -1;
+	private Collider2D mycollider;
 	// Use this for initialization
 	void Start () {
 		myRigidBunny = GetComponent<Rigidbody2D> ();
 		myAnim = GetComponent<Animator> ();
+		mycollider = GetComponent<Collider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonUp ("jump")) {
-			myRigidBunny.AddForce (transform.up * jumpForce);
+		if (bunnyHurtTime == -1) {
+			if (Input.GetButtonUp ("jump")) {
+				myRigidBunny.AddForce (transform.up * jumpForce);
+			}
+			myAnim.SetFloat ("vVelocity", myRigidBunny.velocity.y);
+		} else {
+			if (Time.time > bunnyHurtTime + 2) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+			}
 		}
-		myAnim.SetFloat ("vVelocity", myRigidBunny.velocity.y);
-
 	}
 	void OnCollisionEnter2D(Collision2D bCollision)
 	{
 		if (bCollision.collider.gameObject.layer == LayerMask.NameToLayer ("Enemy")) {
-			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+
+			foreach (CactusMoving cactusMover in FindObjectsOfType<CactusMoving>()) {
+				cactusMover.enabled = false;
+			}
+			foreach (SpawnScript spawner in FindObjectsOfType<SpawnScript>()) {
+				spawner.enabled = false;
+			}
+			bunnyHurtTime = Time.time;
+			myAnim.SetBool ("bunnyHurt", true);
+			myRigidBunny.velocity = Vector2.zero;
+			myRigidBunny.AddForce (transform.up * jumpForce);
+			mycollider.enabled = false;
 		}
 	}
 }
